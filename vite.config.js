67ecf859d4e11be60adddd17e64d5ecce295d49e
汤昊赟 <13033431606@163.com>
2023-10-31 21:13:34 +0800

    change

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
+  base: '/korea-web/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
